7cfdd6dcf52fe31700e9c7c48895a4e0b9e435c6
Alessio Cimarelli <alessio@dataninja.it>
2018-06-21 23:55:34 +0200

    Add scales

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -2,25 +2,66 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import { VictoryContainer, Circle, Rect, Path } from 'victory-core'
 import * as d3Shape from 'd3-shape'
+import * as d3Scale from 'd3-scale'
+import * as d3Array from 'd3-array'
 import './index.scss'
 
 export default class Chart extends Component {
 
+    
+
     render() {
+
+        let component = this
+
+        let scaleX = d3Scale.scalePoint()
+            .domain(d3Array.range(component.props.ns.length))
+            .rangeRound([0,component.props.width])
+            .padding(0.5)
+
+        let scaleY = (y) => Math.floor(y)
+
+        let scaleRadius = d3Scale.scaleSqrt()
+            .domain(d3Array.extent(component.props.ns))
+            .range(
+                [
+                    2,
+                    Math.floor(
+                        Math.min(
+                            component.props.height/2,
+                            component.props.width/component.props.ns.length/2
+                        )
+                    )
+                ]
+            )
+
+        let scaleHeight = d3Scale.scaleLinear()
+            .domain(d3Array.extent(component.props.ns))
+            .range(
+                [
+                    0,
+                    Math.floor(component.props.height/2)
+                ]
+            )
+
+        let line = d3Shape.line()
+            .x((d,i) => scaleX(i))
+            .y(d => scaleY(component.props.height/2)-scaleHeight(d))
+
         return (
-            <VictoryContainer width={this.props.width} height={this.props.height} responsive={true}>
+            <VictoryContainer width={component.props.width} height={component.props.height}>
                 <g>
-                    {this.props.ns.map((r, index) => (
-                        <Circle cx={50*index+25} cy={this.props.height/2} r={r} key={'c'+index}/>
+                    {component.props.ns.map((r, index) => (
+                        <Circle cx={scaleX(index)} cy={scaleY(component.props.height/2)} r={scaleRadius(r)} key={'c'+index}/>
                     ))}
                 </g>
                 <g>
-                    {this.props.ns.map((r, index) => (
-                        <Rect x={50*index+25-2} y={this.props.height/2-r} width={4} height={r} key={'r'+index} style={{fill:"grey"}}/>
+                    {component.props.ns.map((h, index) => (
+                        <Rect x={scaleX(index)-2} y={scaleY(component.props.height/2)-scaleHeight(h)} width={4} height={scaleHeight(h)} key={'r'+index} style={{fill:"grey"}}/>
                     ))}
                 </g>
                 <g>
-                    <Path d={d3Shape.line().x((d,i) => 50*i+25).y(d => this.props.height/2-d)(this.props.ns)} style={{fill:"none",stroke:"steelblue",strokeWidth:"2px"}}/>
+                    <Path d={line(component.props.ns)} style={{fill:"none",stroke:"steelblue",strokeWidth:"2px"}}/>
                 </g>
             </VictoryContainer>
         )
