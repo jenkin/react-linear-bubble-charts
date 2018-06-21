bd5d3ce4f99fd10e391a33dea3c57641d05ebdd2
Alessio Cimarelli <alessio@dataninja.it>
2018-06-22 01:26:15 +0200

    Add labels and titles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Chart from '../Chart'
 import * as d3Array from 'd3-array'
+import * as d3Collection from 'd3-collection'
 import './index.scss'
 
 export default class App extends Component {
@@ -9,43 +10,61 @@ export default class App extends Component {
     constructor(props) {
         super(props)
         let component = this
+
+        component.firstOrderCategories = ["Morocco","Venezuela","Congo","Italy","Puerto Rico","Sri Lanka","Poland","Malaysia","Seychelles","Réunion","Ukraine","Afghanistan","Libya","New Zealand","Mauritius"]
+        component.secondOrderCategories = ["Golf","Volleyball","Swimming","Badminton","Gynastics","Rugby","Ice Hockey","Rowing","Fencing","Surfing"]
+        component.firstFilteredCategories = ["computer","lamp shade","drill press","bananas","tomato","ipod"]
+        component.secondFilteredCategories = ["thermostat","couch","lotion","slipper","key chain","glass"]
+
+        component.data = component.generateData(100)
         component.state = {
-            circles: component.genCircles(10)
+            countries: component.sliceData(100)
         }
+
     }
 
     render() {
+
         return (
             <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <input type="range" min="5" max="25" step="1" defaultValue="10" onChange={this.updateState.bind(this)} />
-                    </div>
-                </div>
-                <div className="row">
-                    {this.state.circles.map((ns, index) => (
-                        <div className="col-6 text-center" key={'d' + index}>
-                            <Chart ns={ns} />
+                {this.state.countries.map((country, index) => (
+                    <div className="row" key={'d' + index}>
+                        <h2 className="col-12 text-center">{country.key}</h2>
+                        <div className="col-12 text-center">
+                            <Chart circles={country.values} />
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         )
     }
 
-    updateState(e) {
-        this.setState({circles:this.genCircles(e.target.value)})
+    sliceData(n) {
+        return d3Collection.nest()
+            .key(d => d.firstOrderCategory)
+            .key(d => d.secondOrderCategory)
+            .entries(this.data.slice(0,n))
     }
 
-    genCircles(n) {
+    updateState(e) {
+        this.setState({
+            countries: this.sliceData(e.target.value)
+        })
+    }
+
+    generateData(n) {
         let component = this
         return d3Array
             .range(n)
             .map(
-                n => d3Array
-                    .range(component.getRandomIntInclusive(3, 10))
-                    .map(n => component.getRandomIntInclusive(5,25)
-                )
+                (n) => ({
+                    "firstOrderCategory": component.firstOrderCategories[Math.floor(Math.random() * component.firstOrderCategories.length)],
+                    "secondOrderCategory": component.secondOrderCategories[Math.floor(Math.random() * component.secondOrderCategories.length)],
+                    "firstFilteredCategory": component.firstFilteredCategories[Math.floor(Math.random() * component.firstFilteredCategories.length)],
+                    "secondFilteredCategory": component.secondFilteredCategories[Math.floor(Math.random() * component.secondFilteredCategories.length)],
+                    "name": "c"+n,
+                    "value": component.getRandomIntInclusive(5,25)
+                })
             )
     }
 
--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import { VictoryContainer, Circle, Rect, Path } from 'victory-core'
+import { VictoryContainer, Circle, Rect, Path, Text } from 'victory-core'
 import * as d3Shape from 'd3-shape'
 import * as d3Scale from 'd3-scale'
 import * as d3Array from 'd3-array'
@@ -8,45 +8,45 @@ import './index.scss'
 
 export default class Chart extends Component {
 
-    
-
     render() {
 
         let component = this
 
+        let values = d3Array.extent(component.props.circles.map(c => c.values.map(v => v.value).reduce((m,n) => m+n,0)))
+
         let scaleX = d3Scale.scalePoint()
-            .domain(d3Array.range(component.props.ns.length))
+            .domain(d3Array.range(component.props.circles.length))
             .rangeRound([0,component.props.width])
             .padding(0.5)
 
         let scaleY = (y) => Math.floor(y)
 
         let scaleRadius = d3Scale.scaleSqrt()
-            .domain(d3Array.extent(component.props.ns))
+            .domain(d3Array.extent(values))
             .range(
                 [
                     2,
                     Math.floor(
                         Math.min(
-                            component.props.height/2,
-                            component.props.width/component.props.ns.length/2
+                            component.props.height/3,
+                            component.props.width/component.props.circles.length/2
                         )
                     )
                 ]
             )
 
         let scaleHeight = d3Scale.scaleLinear()
-            .domain(d3Array.extent(component.props.ns))
+            .domain(d3Array.extent(values))
             .range(
                 [
-                    0,
-                    Math.floor(component.props.height/2)
+                    2,
+                    Math.floor(component.props.height/3)
                 ]
             )
 
         let line = d3Shape.line()
             .x((d,i) => scaleX(i))
-            .y(d => scaleY(component.props.height/2)-scaleHeight(d))
+            .y(d => scaleY(component.props.height/2)-scaleHeight(d.values.map(v => v.value).reduce((m,n) => m+n,0)))
 
         return (
             <VictoryContainer
@@ -55,31 +55,69 @@ export default class Chart extends Component {
                 className = "chart"
             >
                 <g>
-                    {component.props.ns.map((r, index) => (
-                        <Circle
-                            cx = {scaleX(index)}
-                            cy = {scaleY(component.props.height/2)}
-                            r = {scaleRadius(r)}
-                            key = {'c'+index}
-                            className = "bubble"
-                        />
-                    ))}
+                    {component.props.circles.map((c, index) => {
+                        let r = c.values.map(v => v.value).reduce((m,n) => m+n,0)
+                        return (
+                            <Circle
+                                cx = {scaleX(index)}
+                                cy = {scaleY(component.props.height/2)}
+                                r = {scaleRadius(r)}
+                                key = {'c'+index}
+                                className = "bubble"
+                            />
+                        )
+                    })}
                 </g>
                 <g>
-                    {component.props.ns.map((h, index) => (
-                        <Rect
-                            x = {scaleX(index)-2}
-                            y = {scaleY(component.props.height/2)-scaleHeight(h)}
-                            width = {4}
-                            height = {scaleHeight(h)}
-                            key = {'r'+index}
-                            className = "bar"
-                        />
-                    ))}
+                    {component.props.circles.map((c, index) => {
+                        let h = c.values.map(v => v.value).reduce((m,n) => m+n,0)
+                        return (
+                            <Rect
+                                x = {scaleX(index)-2}
+                                y = {scaleY(component.props.height/2)-scaleHeight(h)}
+                                width = {4}
+                                height = {scaleHeight(h)}
+                                key = {'r'+index}
+                                className = "bar"
+                            />
+                        )
+                    })}
+                </g>
+                <g>
+                    {component.props.circles.map((c, index) => {
+                        let r = c.values.map(v => v.value).reduce((m,n) => m+n,0)
+                        return (
+                            <Text
+                                x = {scaleX(index)}
+                                y = {scaleY(component.props.height/2) + scaleRadius(r)}
+                                dy = {8}
+                                key = {'tt'+index}
+                                className = "label"
+                            >
+                                {c.key}
+                            </Text>
+                        )
+                    })}
+                </g>
+                <g>
+                    {component.props.circles.map((c, index) => {
+                        let n = c.values.map(v => v.value).reduce((m,n) => m+n,0)
+                        return (
+                            <Text
+                                x = {scaleX(index)}
+                                y = {scaleY(component.props.height/2)-scaleHeight(n)}
+                                dy = {-4}
+                                key = {'nt'+index}
+                                className = "label"
+                            >
+                                {n}
+                            </Text>
+                        )
+                    })}
                 </g>
                 <g>
                     <Path
-                        d = {line(component.props.ns)}
+                        d = {line(component.props.circles)}
                         className = "line"
                     />
                 </g>
@@ -92,13 +130,11 @@ export default class Chart extends Component {
 Chart.propTypes = {
     width: PropTypes.number,
     height: PropTypes.number,
-    ns: PropTypes.array,
-    r: PropTypes.number,
+    circles: PropTypes.array,
 }
 
 Chart.defaultProps = {
     width: 300,
     height: 50,
-    ns: [1,2,3,4,5],
-    r: 25,
+    circles: [1,2,3,4,5],
 }
